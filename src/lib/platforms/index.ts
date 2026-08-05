5c1cd07136c16afbe1d1c7b0db4673c95197115b
tantenton <cvdv0509@gmail.com>
2026-08-05 14:46:42 +0700

    feat: export all 4 platform connectors in index.ts

--- src/lib/platforms/index.ts
+++ src/lib/platforms/index.ts
@@ -1,3 +1,7 @@
 export { TwitterConnector } from './twitter';
 export { ThreadsConnector } from './threads';
+export { InstagramConnector } from './instagram';
+export { TikTokConnector } from './tiktok';
+export { YouTubeConnector } from './youtube';
+export { FacebookConnector } from './facebook';
 export type { BasePlatformConnector, IPlatformConnector, PostResult } from './types';
